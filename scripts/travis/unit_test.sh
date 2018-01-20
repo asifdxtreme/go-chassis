d69008a9a8c829b11ef3ab4c52920bee5323fad1
asifdxtreme <mohammad.asif.siddiqui1@huawei.com>
2018-01-20 13:51:39 +0530

    Add go vet and unit test in Travis
    Signed-off-by: asifdxtreme <mohammad.asif.siddiqui1@huawei.com>

---
+++ scripts/travis/unit_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+echo "mode: atomic" > coverage.txt
+export COVERAGE_PATH=$(pwd)
+cd $1
+for d in $(go list ./... | grep -v vendor |  grep -v third_party); do
+    cd $GOPATH/src/$d
+    if [ $(ls | grep _test.go | wc -l) -gt 0 ]; then
+        go test -cover -covermode atomic -coverprofile coverage.out
+        if [ -f coverage.out ]; then
+            sed '1d;$d' coverage.out >> $GOPATH/src/github.com/ServiceComb/go-chassis/coverage.txt
+        fi
+    fi
+done
