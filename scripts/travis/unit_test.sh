ea1dd57b8155143f7305b336fb68aba13bc23ebc
asifdxtreme <mohammad.asif.siddiqui1@huawei.com>
2018-01-22 20:11:47 +0530

    Update the Unit test

--- scripts/travis/unit_test.sh
+++ scripts/travis/unit_test.sh
@@ -4,7 +4,7 @@ mkdir -p /go/src/github.com/ServiceComb/go-chassis
 cp -r /go/src/github.com/{{ORG_NAME}}/{{REPO_NAME}}/* /go/src/github.com/ServiceComb/go-chassis/
 cd /go/src/github.com/ServiceComb/go-chassis
 pwd
-ll
+ls
 echo "mode: atomic" > coverage.txt
 #export BUILD_ENV=travis
 mkdir conf
