364a5b17cbc78d606f4798fa6298b7203f54fa86
asifdxtreme <mohammad.asif.siddiqui1@huawei.com>
2018-01-22 18:18:53 +0530

    Update the Unit test

--- scripts/travis/unit_test.sh
+++ scripts/travis/unit_test.sh
@@ -4,10 +4,12 @@ echo "mode: atomic" > coverage.txt
 export BUILD_ENV=travis
 mkdir conf
 cp -r scripts/travis/*.yaml conf/
+mkdir log
+echo " " > log/chassis.log
 for d in $(go list ./... | grep -v vendor |  grep -v third_party); do
     cd $GOPATH/src/$d
     if [ $(ls | grep _test.go | wc -l) -gt 0 ]; then
-        go test -cover -covermode atomic -coverprofile coverage.out
+        sudo go test -cover -covermode atomic -coverprofile coverage.out
         if [ -f coverage.out ]; then
             sed '1d;$d' coverage.out >> $GOPATH/src/github.com/ServiceComb/go-chassis/coverage.txt
         fi
