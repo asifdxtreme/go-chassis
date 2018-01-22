a2e36d716051e6b046b4c0e6ec28079724126faa
asifdxtreme <mohammad.asif.siddiqui1@huawei.com>
2018-01-22 19:29:41 +0530

    Update the Unit test

--- scripts/travis/unit_test.sh
+++ scripts/travis/unit_test.sh
@@ -7,6 +7,8 @@ mkdir log
 cp -r scripts/travis/*.yaml conf/
 pwd
 for d in $(go list ./... | grep -v vendor |  grep -v third_party); do
+    echo $d
+    echo $GOPATH
     cd $GOPATH/src/$d
     if [ $(ls | grep _test.go | wc -l) -gt 0 ]; then
         go test -cover -covermode atomic -coverprofile coverage.out
